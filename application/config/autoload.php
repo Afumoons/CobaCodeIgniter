5743fe86b7482274f33f20c2fe189abb9e57d988
Afu Sidhi Pamekas <afusidhipamekas@gmail.com>
2019-10-20 17:14:04 +0700

    Video pembuatan login dengan CI ke-2 (membuat halaman login dan register) https://www.youtube.com/watch?v=cZKLecON8tA&list=PLFIM0718LjIXU8ul9FiN-owk04cQKtHPw&index=2

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session');
+$autoload['libraries'] = array('database', 'session', 'email');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,8 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url');
+//url buat base_url
+$autoload['helper'] = array('url', 'file', 'security');
 
 /*
 | -------------------------------------------------------------------
