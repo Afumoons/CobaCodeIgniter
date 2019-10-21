420a8cbea49ee474074e09dac4949e0c11c2f3fe
Afu Sidhi Pamekas <afusidhipamekas@gmail.com>
2019-10-21 09:35:15 +0700

    Video ke-3 (Menyelesaikan fitur registration) https://www.youtube.com/watch?v=InxCCPlKCrk&list=PLFIM0718LjIXU8ul9FiN-owk04cQKtHPw&index=3

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -2,16 +2,49 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
     public function index()
     {
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'Login Page';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
     public function registration()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+
+        $this->form_validation->set_rules('username', 'Username', 'required|trim|is_unique[user.username]', [
+            'is_unique' => 'This username has already used'
+        ]);
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.user_email]', [
+            'is_unique' => 'This email has already registered'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[8]|matches[password2],', [
+            'matches' => 'Password dont match!', 'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Repeat Password', 'required|trim|matches[password1]');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Registration Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'username' => $this->input->post('username'),
+                'user_email' => $this->input->post('email'),
+                'user_password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                'is_active' => 1,
+                'role_id' => 2,
+                'user_datecreated' => time(),
+                'user_image' => 'default.jpg'
+            ];
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', ' <div class="alert alert-success" role="alert"><strong>Congratulation! you account has been created. Please Login</strong></div>');
+            redirect('auth');
+        }
     }
 }
--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -9,21 +9,19 @@
                         <div class="text-center">
                             <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                         </div>
-                        <form class="user">
-                            <div class="form-group row">
-                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="text" class="form-control form-control-user" id="fname" name="fname" placeholder="First Name">
-                                </div>
-                                <div class="col-sm-6">
-                                    <input type="text" class="form-control form-control-user" id="lname" name="lname" placeholder="Last Name">
-                                </div>
+                        <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Username" value="<?= set_value('username'); ?>">
+                                <?= form_error('username', '<small id="helpId" class="text-danger form-text pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                                <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email Address" value="<?= set_value('email'); ?>">
+                                <?= form_error('email', '<small id="helpId" class="text-danger form-text pl-3">', '</small>'); ?>
                             </div>
                             <div class="form-group row">
                                 <div class="col-sm-6 mb-3 mb-sm-0">
                                     <input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password">
+                                    <?= form_error('password1', '<small id="helpId" class="text-danger form-text pl-3">', '</small>'); ?>
                                 </div>
                                 <div class="col-sm-6">
                                     <input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
